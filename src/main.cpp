e51a76f156ea7ed195c8f703b92a0eff3e3affa8
sofa <146020160+sofabeddd@users.noreply.github.com>
2024-03-06 18:33:31 -0600

    i just shitted out my ass

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(GeometryDashMenuLayer, MenuLayer) {
+public:
+    static void onModify(auto& self) {
+        (void) self.setHookPriority("MenuLayer::init", -10000);
+    }
+
+    void onMainTitle(CCObject* sender) {
+        if (typeinfo_cast<CCMenuItemSpriteExtra*>(sender)) {
+            FMODAudioEngine::sharedEngine()->playEffect("geometry-dash.ogg"_spr, 1.f, 1.f, 1.f);
+        }
+    }
+
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        auto main_title = getChildByID("main-title");
+        main_title->removeFromParent();
+
+        auto main_menu = getChildByID("main-menu");
+
+        auto main_title_menu= CCMenu::create();
+        main_title_menu->setContentSize(CCSize { 429.f, 52.f });
+        main_title_menu->setPosition(main_menu->boundingBox().origin.x, 270 - 26);
+
+        auto title_menu_button = CCMenuItemSpriteExtra::create(main_title, this, menu_selector(GeometryDashMenuLayer::onMainTitle));
+        title_menu_button->setPosition(214.5, 26.f);
+
+        main_title_menu->addChild(title_menu_button);
+        addChild(main_title_menu);
+
+        return true;
+    }
+};
